2cdd0c5e1329573d7dd050ecebfbc8e32f7fca76
Panayiotis Halouvas <phalouvas@gmail.com>
2026-01-27 10:47:32 +0200

    Update release version to 1.4 in build script

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Release version - update this for new releases
-RELEASE_VERSION="1.3"
+RELEASE_VERSION="1.4"
 BUILD_MANIFEST=".build_manifest"
 
 echo "=========================================="
