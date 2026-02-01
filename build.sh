e4f626fe6c133f9df7ea103c4afeeb6e8c15a2ab
Panayiotis Halouvas <phalouvas@gmail.com>
2026-02-01 07:48:45 +0200

    Update release version to 1.6 in build script

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Release version - update this for new releases
-RELEASE_VERSION="1.5"
+RELEASE_VERSION="1.6"
 BUILD_MANIFEST=".build_manifest"
 
 echo "=========================================="
