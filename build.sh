02c281778976f31519bbea1505a412a4f692a3ae
Panayiotis Halouvas <phalouvas@gmail.com>
2026-02-06 09:35:52 +0200

    Update release version to 1.11 in build script

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Release version - update this for new releases
-RELEASE_VERSION="1.10"
+RELEASE_VERSION="1.11"
 BUILD_MANIFEST=".build_manifest"
 
 echo "=========================================="
